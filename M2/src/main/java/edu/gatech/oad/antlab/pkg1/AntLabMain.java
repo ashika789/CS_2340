f32a92ac3af4cb1244c302f30bec27308f8af214
Tucker LoCicero <t.g.locicero@gmail.com>
2015-08-31 13:10:55 -0400

    Updated person4 information
    see title

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,14 +67,14 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Saqlain Golandaz");
 		  toPrint += p3.toString("sgolandaz3");
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
-                  //Person5 replace P4 with your name
-                  //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+          //Person4 replace P4 with your name
+          //and gburdell4 with your gt id
+	      Person4 p4 = new Person4("Tucker LoCicero");
+          toPrint += p4.toString("tlocicero3");
+          //Person5 replace P4 with your name
+          //and gburdell5 with your gt id
+          Person5 p5 = new Person5("P5");
+          toPrint += p5.toString("gburdell5");
 		  
         System.out.println(toPrint);
          
