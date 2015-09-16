b7fb3ddf27097cbe2e1885d4a9d7e951e6757f2d
stithwilliam <stithwilliam@gmail.com>
2015-09-16 02:20:49 -0400

    Failed attempt at comboBoxes and radioButtons
    Cleaned up the code from the last push. Cannot get my comboBoxes to fill
    with entries or the radioButtons to group up so that only one can be
    pressed at a time.

--- MULE/src/configScreensM3/ConfigController.java
+++ MULE/src/configScreensM3/ConfigController.java
@@ -1,49 +1,46 @@
 package configScreensM3;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class ConfigController {
 
     @FXML
-    private Button playBtn;
+    private Button playBtn, playScrnNext, playScrnBack, gameStart, playerConfigBack;
+
     @FXML
-    private Button playScrnNext;
+    private RadioButton easyRadio, MediumRadio, hardRadio;
     @FXML
-    private Button playScrnBack;
+    private RadioButton standardMapRadio, randomMapRadio;
+
     @FXML
-    private Button gameStart;
-    @FXML
-    private Button playerConfigBack;
+    private ComboBox<String> combo1human, combo1race, combo2human, combo2race, combo3human, combo3race, combo4human, combo4race;
+
+    ObservableList<String> humanBox = FXCollections.observableArrayList("Human", "AI", "Not playing");
+    ObservableList<String> raceBox = FXCollections.observableArrayList("Human", "Flapper", "Bonzoid", "Ugaite", "Buzzite");
 
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException {
         Stage stage;
         Parent root;
-        if (event.getSource() == playBtn) {
-            stage = (Stage) playBtn.getScene().getWindow();
+        Button source = (Button) event.getSource();
+        stage = (Stage) source.getScene().getWindow();
+        if (source == playBtn || source == playerConfigBack) {
             root = FXMLLoader.load(getClass().getResource("playScreen.fxml"));
-        } else if (event.getSource() == playerConfigBack) {
-            stage = (Stage) playerConfigBack.getScene().getWindow();
-            root = FXMLLoader.load(getClass().getResource("playScreen.fxml"));
-        } else if (event.getSource() == playScrnNext) {
-            stage = (Stage) playScrnNext.getScene().getWindow();
+        } else if (source == playScrnNext) {
             root = FXMLLoader.load(getClass().getResource("playerConfigScreen.fxml"));
-        } else if (event.getSource() == playScrnBack) {
-            stage = (Stage) playScrnBack.getScene().getWindow();
+        } else if (source == playScrnBack) {
             root = FXMLLoader.load(getClass().getResource("home.fxml"));
         } else {
-            stage = (Stage) playerConfigBack.getScene().getWindow();
             root = FXMLLoader.load(getClass().getResource("gamePlaceHolderScreen.fxml"));
         }
         Scene scene = new Scene(root);
@@ -51,7 +48,33 @@ public class ConfigController {
         stage.show();
     }
 
-    public void initialize(URL url, ResourceBundle rb) {
+//    @FXML
+//    private void handleComboBoxes(ActionEvent event) throws IOException {
+//        System.out.println("handling combo box");
+//        ComboBox<String> source = (ComboBox) event.getSource();
+//        if (source == combo1human) {
+//            combo1human.setItems(humanBox);
+//        }
+//    }
 
+//    @FXML  //Trying to deal with the radio buttons on playerConfigScreen. Not working
+//    private void handleDifficultyRadio(ActionEvent event) throws IOException {
+//        RadioButton source = (RadioButton) event.getSource();
+//        RadioButton[] radios = {easyRadio, mediumRadio, hardRadio};
+//        for (RadioButton x : radios) {
+//            if (x != source) {
+//                x.fire();
+//            }
+//        }
+//    }
+
+//    @FXML needs to deal with the map radios
+//    private void handleMapRadio(ActionEvent event) throws IOException {
+//
+//    }
+
+    public void initialize() throws IOException {
+        //combo1human.setItems(humanBox);  //trying to set up the combobox on the playerConfigScreen, not working.
+        //combo1race.setItems(raceBox);
     }
 }
--- MULE/src/configScreensM3/Main.java
+++ MULE/src/configScreensM3/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
         primaryStage.setTitle("MULE");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
