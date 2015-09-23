442f5499b565ff1de5bf14df03e5e0953070d40d
ynambiar <yamini.nambiar@gmail.com>
2015-09-23 13:21:23 -0400

    StandardMapController

--- MULE/src/game/StandardMapController.java
+++ MULE/src/game/StandardMapController.java
@@ -11,9 +11,28 @@ import javafx.stage.Popup;
 import javafx.stage.Stage;
 import configScreens.Config2Controller;
 
+import java.io.IOException;
+
 /**
  * Created by tuckerlocicero on 9/23/15.
  */
 public class StandardMapController {
+
+    @FXML
+    private Button button00, button01, button02, button03, button04, button05, button06, button07, button08, button09;
+
+    @FXML
+    private void okayButtonAction(ActionEvent event) throws IOException {
+        Stage stage;
+        Button source = (Button) event.getSource();
+        stage = (Stage) source.getScene().getWindow();
+        if (source == okayButton) {
+            stage.close();
+        }
+    }
+
+
+    public void initialize() throws IOException {
+    }
 
 }
