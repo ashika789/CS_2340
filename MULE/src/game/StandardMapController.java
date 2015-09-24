5268a074bfcc06eaf402315fa88f9fd5da9b4e70
saqlaingolandaz <saqlaingolandaz@hotmail.com>
2015-09-24 16:20:29 -0400

    selecting tiles

--- MULE/src/game/StandardMapController.java
+++ MULE/src/game/StandardMapController.java
@@ -7,12 +7,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Modality;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
 import configScreens.Config2Controller;
 
+import javax.swing.text.html.ImageView;
+import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -29,10 +32,12 @@ public class StandardMapController {
     private Button btn20, btn21, btn22, btn23, btn24, btn25, btn26, btn27, btn28;
     @FXML
     private Button btn30, btn31, btn32, btn33, btn34, btn35, btn36, btn37, btn38;
+    @FXML
+    private ImageView button00;
 
     int[][] mapOfbtns = new int[5][9]; //initialized to 5x9 grid of 0s, 1 means btn is pressed
 
-    @FXML
+
 
 /* This is the logic to loop through the Players to purchase property at the g*/
 // for (int i = 0; i < Config2Controller.gameMap.players.size() * 2; i++) {
@@ -46,45 +51,13 @@ public class StandardMapController {
 //        }
 //    }
 
+    @FXML
     private void buttonHandler(ActionEvent event) throws IOException {
-        Button source = (Button) event.getSource();
-        if (source == btn00) {
+        ImageView source = (ImageView) event.getSource();
+        if (source.equals(button00)) {
             System.out.println("this works0");
-        } else if (source == btn01) {
-            System.out.println("this works1");
-        } else if (source == btn02) {
-            System.out.println("this works2");
-        } else if (source == btn03) {
-            System.out.println("this works3");
-        } else if (source == btn04) {
-            System.out.println("this works4");
-        } else if (source == btn05) {
-            System.out.println("this works5");
-        } else if (source == btn06) {
-            System.out.println("this works6");
-        } else if (source == btn07) {
-            System.out.println("this works7");
-        } else if (source == btn08) {
-            System.out.println("this works8");
-        } else if (source == btn10) {
-            System.out.println("this works9");
-        } else if (source == btn11) {
-            System.out.println("this works10");
-        } else if (source == btn12) {
-            System.out.println("this works11");
-        } else if (source == btn13) {
-            System.out.println("this works12");
-        } else if (source == btn14) {
-            System.out.println("this works13");
-        } else if (source == btn15) {
-            System.out.println("this works14");
-        } else if (source == btn16) {
-            System.out.println("this works15");
-        } else if (source == btn17) {
-            System.out.println("this works16");
-        } else if (source == btn18) {
-            System.out.println("this works17");
-        }
+            Config2Controller.hasSelected = false;
+        } 
     }
 
     @FXML
