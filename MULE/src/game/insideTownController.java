88d82ae47c225611703fb04e91db9215f68da1df
Tucker LoCicero <t.g.locicero@gmail.com>
2015-09-24 20:57:31 -0400

    Made back button for town, added Controller for insideTown

---
+++ MULE/src/game/insideTownController.java
@@ -0,0 +1,35 @@
+package game;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Created by tuckerlocicero on 9/24/15.
+ */
+public class insideTownController {
+
+    @FXML
+    private Button leaveTownBtn;
+
+    @FXML
+    private void buttonHandler(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent root = null;
+        Button source = (Button) event.getSource();
+        stage = (Stage) source.getScene().getWindow();
+        if (source == leaveTownBtn) {
+            root = FXMLLoader.load(getClass().getResource("StandardMap.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+}
