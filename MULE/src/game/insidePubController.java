07d073defe2e3026631fe6631d24eb988bfdd044
Tucker LoCicero <t.g.locicero@gmail.com>
2015-10-01 16:33:51 -0400

    NOW the pub controller is done

--- MULE/src/game/insidePubController.java
+++ MULE/src/game/insidePubController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,19 +20,24 @@ public class insidePubController extends configScreens.ControllerSuper{
     private Button noGambleBtn;
 
     @FXML
+    private Button doneGamblingBtn;
+
+    @FXML
+    private Label resultsLabel;
+
+    @FXML
     private void buttonHandler(ActionEvent event) throws IOException {
         Stage stage;
         Parent root = null;
         Button source = (Button) event.getSource();
         stage = (Stage) source.getScene().getWindow();
         if (source == yesGambleBtn) {
-            /*
             int time = driver.getTimeLeft();
             if (time >= 37) {
                 time = 200;
-            } else if (37 > time >= 25) {
+            } else if (37 > time && time >= 25) {
                 time = 150;
-            } else if (25 > time >= 12) {
+            } else if (25 > time && time >= 12) {
                 time = 100;
             } else {
                 time = 50;
@@ -42,13 +48,18 @@ public class insidePubController extends configScreens.ControllerSuper{
             int bonus = new Random().nextInt(time) + roundBonuses[round - 1];
             driver.getPlayer().addMoney(bonus);
             //Display money earned screen TODO
-            driver.startTurn();
-            */
+            root = FXMLLoader.load(getClass().getResource("insidePub.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            resultsLabel.setText(Integer.toString(bonus));
         } else if (source == noGambleBtn) {
             root = FXMLLoader.load(getClass().getResource("insideTown.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
+        } else if (source == doneGamblingBtn) {
+            Driver.endTurn();
         }
     }
 }
