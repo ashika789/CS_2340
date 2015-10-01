e01afe0e5064223f8efadcb24ada7f39b55aee9e
ashika789 <ashika789@gmail.com>
2015-09-30 22:11:36 -0400

    Revert "round based on score"
    This reverts commit 4719931d7c4cd5a870016427c74fcf6d071e54a0.

--- MULE/src/game/Player.java
+++ MULE/src/game/Player.java
@@ -22,10 +22,6 @@ public class Player {
     private int playerNumber;
     private int money;
 
-
-
-    private int score;
-
     //default ctor
     //default ctor's playerNumber is 0. Not sure why we even need a default character
     public Player() {
@@ -76,8 +72,6 @@ public class Player {
         } else if (race == "Human") {
             money = 600;
         }
-
-        score = 0;
     }
 
     /* PlayerComparator class definition*/
@@ -85,7 +79,7 @@ public class Player {
         @Override
         //sorts based on money, from greatest to least
         public int compare(Player a, Player b) {
-            return b.getScore() - a.getScore();
+            return b.getMoney() - a.getMoney();
         }
     }
 
@@ -152,17 +146,5 @@ public class Player {
     public int getMoney() { return money; }
 
     public void addMoney(int m) { money += m; }
-
-    public int getScore() {;
-        score += getMoney() + properties.size() * 1500 + getEnergy() * 25 + getFood() *30 + getSmithore() * 50 + getMule() * 100 ;
-        return score;
-    }
-
-    public void setScore(int score) { this.score = score; }
-
-
-    /*1 point per dollar of money
-    500 points for each plot of land owned
-    1 point per dollar value of goods (energy, food, ore)*/
 
 }
