c05bc63079d77e9589df25fd321c46993f06b251
Tucker LoCicero <t.g.locicero@gmail.com>
2015-10-01 16:14:07 -0400

    Finished Gambling Results Screen

--- MULE/src/game/insidePubController.java
+++ MULE/src/game/insidePubController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,13 +20,18 @@ public class insidePubController extends configScreens.ControllerSuper{
     private Button noGambleBtn;
 
     @FXML
+    private Button doneGamblingBtn;
+
+    @FXML
+    private Label resultsLabel;
+
+    @FXML
     private void buttonHandler(ActionEvent event) throws IOException {
         Stage stage;
         Parent root = null;
         Button source = (Button) event.getSource();
         stage = (Stage) source.getScene().getWindow();
         if (source == yesGambleBtn) {
-            /*
             int time = driver.getTimeLeft();
             if (time >= 37) {
                 time = 200;
@@ -43,12 +49,14 @@ public class insidePubController extends configScreens.ControllerSuper{
             driver.getPlayer().addMoney(bonus);
             //Display money earned screen TODO
             driver.startTurn();
-            */
+            resultsLabel.setText(Integer.toString(bonus));
         } else if (source == noGambleBtn) {
             root = FXMLLoader.load(getClass().getResource("insideTown.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
+        } else if (source == doneGamblingBtn) {
+            Driver.endTurn();
         }
     }
 }
